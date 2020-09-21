6655d6c292ed88ba973f2ab7bcc0893b9856c85d
stefanovallarella <stefanovallarella@gmail.com>
2020-09-21 17:23:17 -0300

    Se añade nodemailer y formController  para envío automatico de Emails via Form

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
+const controller = require("../controllers/formController");
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+/* Ruta formulario */
+router.post("/email", controller.index );
 
 module.exports = router;
