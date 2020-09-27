a622d9beacfa29fa19abbf6b01ab9d38b476b143
stefanovallarella <stefanovallarella@gmail.com>
2020-09-27 17:53:43 -0300

    Animaciones product Cards aplicadas, ajustar tiempos de transicion

--- public/javascripts/script-menu.js
+++ public/javascripts/script-menu.js
@@ -1,3 +1,6 @@
+window.addEventListener("load", function(){
+
+
 let menu = document.getElementById("menu");
 let btn = document.getElementById("btn-menu");
 
@@ -10,3 +13,39 @@ btn.addEventListener("click", function(e){
     }
 
 });
+
+
+let productCard = document.getElementById("card-1");
+let arrowCard = document.getElementById("arrow-1");
+let productInfo = document.getElementById("product-1");
+let imagenOne = document.getElementById("image-1");
+
+    productCard.addEventListener("mouseover", function(){
+        arrowCard.style.opacity = 1;
+        imagenOne.style.opacity = .6;
+        /* imagenOne.style.transition = .3; */
+    })
+
+    productCard.addEventListener("click", function(){
+        arrowCard.style.opacity = 0;
+        productInfo.style.opacity = 1;
+        imagenOne.style.opacity = .1;
+    })
+
+    productCard.addEventListener("mouseout", function(){
+            productInfo.style.opacity = 0;
+            arrowCard.style.opacity = 0;
+            imagenOne.style.opacity = 1;
+        })
+
+
+
+
+
+
+
+
+
+
+
+})
